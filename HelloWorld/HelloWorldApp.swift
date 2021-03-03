c51a16c093b269cae05ea13d8d17bd916d661975
AntonAsmirko <LoLkingXXX@gmail.com>
2021-03-04 00:50:05 +0300

    Initial Commit

---
+++ HelloWorld/HelloWorldApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloWorldApp.swift
+//  HelloWorld
+//
+//  Created by Антон Асмирко on 04.03.2021.
+//
+
+import SwiftUI
+
+@main
+struct HelloWorldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
