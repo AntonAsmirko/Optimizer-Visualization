38f26afa5a40de63999b559f1cc0e89a3926bfec
AntonAsmirko <LoLkingXXX@gmail.com>
2021-03-11 08:10:12 +0300

    .

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -14,25 +14,23 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Paint
 import androidx.compose.ui.input.pointer.pointerMoveFilter
 import androidx.compose.ui.layout.WithConstraints
-import androidx.compose.ui.platform.AmbientDensity
-import androidx.compose.ui.platform.DensityAmbient
 import androidx.compose.ui.unit.dp
 import model.PlotData
 import model.interpolators.DichotomyInterpolator
 import kotlin.math.pow
 
 object Constants {
-    const val POINT_RADIUS = 5f
-    const val L_BOUND = -500.0f
-    const val R_BOUND = 500.0f
-    const val NUM_POINTS = 200
+    const val POINT_RADIUS = 4f
+    const val L_BOUND = -50f
+    const val R_BOUND = 0f
+    const val NUM_POINTS = 100
 }
 
 val buttonsText = listOf("Brent", "Dichotomy", "Fibonacci", "Golden Section", "Parabolas")
 val dichotomyInterpolator = DichotomyInterpolator()
 val points =
     dichotomyInterpolator.makePoints(Constants.L_BOUND.toDouble(), Constants.R_BOUND.toDouble(), Constants.NUM_POINTS) {
-        it.pow(2.0)
+        it.pow(2.0) * 0.05
     }
 val maxFnVal = points.maxByOrNull { it.y }?.y ?: -100f
 val minFnVal = points.minByOrNull { it.y }?.y ?: -1000f
@@ -109,7 +107,7 @@ fun plotView(plotData: PlotData, height: Int, width: Int) {
         Paint()
             .apply {
                 color = Color.Red
-                strokeWidth = 2f
+                strokeWidth = 1f
             }
     }
     Canvas(modifier = Modifier, onDraw = {
@@ -122,17 +120,37 @@ fun plotView(plotData: PlotData, height: Int, width: Int) {
                 plotData.lBound,
                 plotData.rBound
             )
-            drawPoints(plotData.points, Constants.POINT_RADIUS, paint, height.toFloat() / 2f, width.toFloat() / 2f)
+            drawGrid(paint, height.toFloat(), width.toFloat())
+            drawPoints(plotData.points, Constants.POINT_RADIUS, paint)
+            scale(1.5f,1.5f)
         }
     })
+}
+
+fun Canvas.drawGrid(paint: Paint, height: Float, width: Float){
+    val step = 50f
+    paint.apply {
+        color = Color(0xff1faa00)
+        strokeWidth = 1f
+    }
+    var curHeight = height / 2
+    while (curHeight > -1 * height / 2) {
+        drawLine(Offset(0f, 0f + curHeight), Offset(width / 2f, 0f + curHeight), paint)
+        drawLine(Offset(0f, 0f + curHeight), Offset(-1 * width / 2f, 0f + curHeight), paint)
+        curHeight -= step
+    }
+    var curWidth = 0f
+    while (curWidth < width / 2){
+        drawLine(Offset(curWidth, height / 2), Offset(curWidth, -1 * height / 2), paint)
+        drawLine(Offset(-curWidth, height / 2), Offset(-curWidth, -1 * height / 2), paint)
+        curWidth += step
+    }
 }
 
 fun Canvas.drawPoints(
     points: List<Offset>,
     radius: Float,
-    paint: Paint,
-    verticalOffset: Float,
-    horizontalOffset: Float
+    paint: Paint
 ) {
     val sorted = points.sortedWith(Comparator { o1, o2 ->
         return@Comparator when {
@@ -143,8 +161,11 @@ fun Canvas.drawPoints(
     }).toTypedArray()
     var prevOffset: Offset? = null
     for (i in sorted.indices) {
-        val curOffset = Offset(sorted[i].x + horizontalOffset, sorted[i].y / 20f + verticalOffset)
-        this.drawCircle(curOffset, radius, paint)
+        val curOffset = Offset(sorted[i].x, sorted[i].y)
+        this.drawCircle(curOffset, radius, paint.apply {
+            color = Color(0xff64dd17)
+            strokeWidth = 7f
+        })
         if (prevOffset != null) {
             this.drawLine(prevOffset, curOffset, paint)
         }
@@ -160,8 +181,8 @@ fun Canvas.prepareYAxis(
     lBound: Float,
     rBound: Float
 ) {
-    this.translate(0f, height)
+    this.translate(width / 2f - lBound, height / 2f)
     this.scale(1f, -1f)
-//    this.translate(lBound, 0f)
-//    this.scale((rBound - lBound) / width, (maxFnVal - minFnVal) / height)
+    //this.translate(lBound, 0f)
+    //this.scale((rBound - lBound) / width, (maxFnVal - minFnVal) / height)
 }
