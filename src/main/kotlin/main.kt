dd7bd5adda443be0bea15c501ec81fe18203f38c
AntonAsmirko <LoLkingXXX@gmail.com>
2021-03-12 09:16:03 +0300

    yqy

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Canvas
@@ -17,29 +18,52 @@ import androidx.compose.ui.layout.WithConstraints
 import androidx.compose.ui.unit.dp
 import model.PlotData
 import model.interpolators.DichotomyInterpolator
+import kotlin.math.log2
 import kotlin.math.pow
+import kotlin.math.sin
 
 object Constants {
     const val POINT_RADIUS = 0.5f
-    const val L_BOUND = -10f
-    const val R_BOUND = 10f
+    const val L_BOUND = 1f
+    const val R_BOUND = 6f
     const val NUM_POINTS = 100
+    const val TITLE = "Optimizer Visualizer"
 }
 
-val buttonsText = listOf("Brent", "Dichotomy", "Fibonacci", "Golden Section", "Parabolas")
+enum class LeftViewType {
+    METHOD,
+    FUNCTION
+}
 
-val dichotomyInterpolator = DichotomyInterpolator()
-val points =
-    dichotomyInterpolator.makePoints(Constants.L_BOUND.toDouble(), Constants.R_BOUND.toDouble(), Constants.NUM_POINTS) {
-        it.pow(3.0)
-    }
-val maxFnVal = points.maxByOrNull { it.y }?.y ?: -100f
-val minFnVal = points.minByOrNull { it.y }?.y ?: -1000f
+enum class MethodType {
+    BRENT,
+    DICHOTOMY,
+    FIBONACCI,
+    GOLDEN_SECTION,
+    PARABOLAS,
+    NONE
+}
 
-val samplePlotData = PlotData(Constants.L_BOUND, Constants.R_BOUND, points, minFnVal, maxFnVal)
+val methodsButtonsText = listOf(
+    Pair("Brent", MethodType.BRENT),
+    Pair("Dichotomy", MethodType.DICHOTOMY),
+    Pair("Fibonacci", MethodType.FIBONACCI),
+    Pair("Golden Section", MethodType.GOLDEN_SECTION),
+    Pair("Parabolas", MethodType.PARABOLAS)
+)
 
-fun main() = Window {
+val functionsButtonsText = mapOf(
+    Pair("y = x", fun(x: Double): Double { return x }),
+    Pair("y = x^2", fun(x: Double): Double { return x.pow(2.0) }),
+    Pair("y = x^3", fun(x: Double): Double { return x.pow(3.0) }),
+    Pair("y = sin(x)", fun(x: Double): Double { return sin(x) }),
+    Pair("y = log2(x)", fun(x: Double): Double { return log2(x) })
+)
+
+fun main() = Window(title = Constants.TITLE) {
     MaterialTheme {
+        var leftViewType by remember { mutableStateOf(LeftViewType.METHOD) }
+        var func by remember { mutableStateOf("none") }
         Row(
             modifier = Modifier
                 .background(color = Color(0xff795548))
@@ -51,23 +75,65 @@ fun main() = Window {
                     .background(color = Color(0xff4b2c20))
                     .fillMaxHeight().border(border = BorderStroke(1.dp, color = Color.White))
             ) {
-                buttonsText.forEach { buttonInBox(it) }
+                if (leftViewType == LeftViewType.METHOD) {
+                    methodsButtonsText.forEach {
+                        buttonInBox(it.first) {
+                            leftViewType = it
+                        }
+                    }
+                } else if (leftViewType == LeftViewType.FUNCTION) {
+                    buttonInBox("Back") {
+                        leftViewType = LeftViewType.METHOD
+                        func = "none"
+                    }
+                    functionsButtonsText.forEach { pair ->
+                        buttonInBox(pair.key) { func = pair.key }
+                    }
+                }
             }
-            WithConstraints(
-                modifier = Modifier
-                    .weight(0.8f)
-                    .fillMaxWidth()
-            ) {
-                val boxWidth = constraints.maxWidth
-                val boxHeight = constraints.maxHeight
-                plotView(samplePlotData, boxHeight, boxWidth)
+            if (func != "none") {
+                WithConstraints(
+                    modifier = Modifier
+                        .weight(0.8f)
+                        .fillMaxWidth()
+                ) {
+                    val boxWidth = constraints.maxWidth
+                    val boxHeight = constraints.maxHeight
+                    val dichotomyInterpolator = DichotomyInterpolator()
+                    val points =
+                        dichotomyInterpolator.makePoints(
+                            Constants.L_BOUND.toDouble(),
+                            Constants.R_BOUND.toDouble(),
+                            Constants.NUM_POINTS,
+                            functionsButtonsText[func]!!
+                        )
+                    val minFnVal: Float = points.minByOrNull { it.y }?.y ?: -100f
+                    val maxFnVal = points.maxByOrNull { it.y }?.y ?: 100f
+                    val samplePlotData = PlotData(Constants.L_BOUND, Constants.R_BOUND, points, minFnVal, maxFnVal)
+                    plotView(samplePlotData, boxHeight, boxWidth)
+                }
+            } else {
+                Column(
+                    Modifier
+                        .align(alignment = Alignment.CenterVertically)
+                        .weight(0.8f)
+                ) {
+                    Text(
+                        modifier = Modifier
+                            .padding(30.dp),
+                        text = "Something will be drawn here after you chose method and function"
+                    )
+                }
             }
         }
     }
 }
 
 @Composable
-fun buttonInBox(text: String) {
+fun buttonInBox(
+    text: String,
+    onChange: (type: LeftViewType) -> Unit
+) {
     val active = remember { mutableStateOf(false) }
     Box(
         modifier = Modifier
@@ -76,7 +142,7 @@ fun buttonInBox(text: String) {
             .fillMaxWidth()
     ) {
         OutlinedButton(
-            onClick = {},
+            onClick = { onChange(LeftViewType.FUNCTION) },
             shape = RoundedCornerShape(50),
             border = BorderStroke(2.dp, Color(0xff64dd17)),
             modifier = Modifier
